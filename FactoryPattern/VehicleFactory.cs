c0aee7538a27c33c1400ad16471e0d8a8c5daa58
Cmarsman <chris_marsden@yahoo.com>
2024-05-16 14:26:43 -0500

    made a change

--- FactoryPattern/VehicleFactory.cs
+++ FactoryPattern/VehicleFactory.cs
@@ -8,13 +8,13 @@ namespace FactoryPattern
 {
     public static class VehicleFactory
     {
-        public static IVehicle GetVehicle(string wheelCount) 
+        public static IVehicle GetVehicle(int wheelCount) 
         {
             switch (wheelCount) 
             {
-                case "2": 
+                case 2: 
                     return new Motorcycle();
-                case "4": 
+                case 4: 
                     return new Car();
                 default:
                     return new Car();
